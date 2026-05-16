bdf6466aae2865ca13d6705fdb8151ca5dbf7397
Murilo Mothsin <mothsin@hotmail.com>
2026-05-16 05:12:47 -0300

    Update Weather specs

--- spec/services/weather_spec.rb
+++ spec/services/weather_spec.rb
@@ -116,4 +116,91 @@ RSpec.describe Weather do
       end
     end
   end
+
+  describe '#complete_weather' do
+    let(:latitude) { 40.7128 }
+    let(:longitude) { -74.0060 }
+    let(:mock_client) { instance_double(OpenWeather::Client) }
+    let(:forecast_data) do
+      {
+        'list' => [
+          { 'main' => { 'temp' => 15 }, 'weather' => [{ 'description' => 'cloudy' }] },
+          { 'main' => { 'temp' => 18 }, 'weather' => [{ 'description' => 'sunny' }] }
+        ]
+      }
+    end
+
+    before do
+      allow(OpenWeather::Client).to receive(:new).and_return(mock_client)
+      allow(mock_client).to receive(:five_day_forecast).and_return(forecast_data)
+    end
+
+    it 'returns forecast data for given latitude and longitude' do
+      weather = Weather.new
+      data = weather.complete_weather(latitude, longitude)
+
+      expect(data).to eq(forecast_data)
+    end
+
+    it 'calls the client with correct parameters' do
+      weather = Weather.new
+      expect(mock_client).to receive(:five_day_forecast).with(lat: latitude, lon: longitude, units: 'metric').and_return(forecast_data)
+
+      weather.complete_weather(latitude, longitude)
+    end
+
+    it 'uses correct cache key format' do
+      weather = Weather.new
+      cache_key = "weather/lat/#{latitude}/lon/#{longitude}/unit/metric"
+
+      expect(Rails.cache).to receive(:fetch).with(cache_key, expires_in: 30.minutes).and_call_original
+
+      weather.complete_weather(latitude, longitude)
+    end
+
+    it 'respects custom units in cache key' do
+      weather = Weather.new(units: 'imperial')
+      cache_key = "weather/lat/#{latitude}/lon/#{longitude}/unit/imperial"
+
+      expect(Rails.cache).to receive(:fetch).with(cache_key, expires_in: 30.minutes).and_call_original
+
+      weather.complete_weather(latitude, longitude)
+    end
+
+    it 'passes custom units to the client' do
+      weather = Weather.new(units: 'imperial')
+      expect(mock_client).to receive(:five_day_forecast).with(lat: latitude, lon: longitude, units: 'imperial').and_return(forecast_data)
+
+      weather.complete_weather(latitude, longitude)
+    end
+
+    context 'with cache enabled' do
+      before do
+        allow(Rails.cache).to receive(:fetch) do |key, options, &block|
+          @test_cache ||= {}
+          if @test_cache.key?(key)
+            @test_cache[key]
+          else
+            @test_cache[key] = block.call
+          end
+        end
+      end
+
+      it 'caches the result' do
+        weather = Weather.new
+        expect(mock_client).to receive(:five_day_forecast).once.with(lat: latitude, lon: longitude, units: 'metric').and_return(forecast_data)
+
+        weather.complete_weather(latitude, longitude)
+        weather.complete_weather(latitude, longitude)
+      end
+
+      it 'uses different cache keys for different coordinates' do
+        weather = Weather.new
+        expect(mock_client).to receive(:five_day_forecast).twice.and_return(forecast_data)
+
+        weather.complete_weather(40.7128, -74.0060)
+        weather.complete_weather(34.0522, -118.2437)
+      end
+    end
+  end
 end
